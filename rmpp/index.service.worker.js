9bcc2f7a5ffaaa067ad3d6994666c1ee571938e9
Kyle Kailihiwa <kylekailihiwa@gmail.com>
2025-03-29 13:58:18 +0800

    rmpp 2.0.2

--- rmpp/index.service.worker.js
+++ rmpp/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743227405|8943141517';
+const CACHE_VERSION = '1743227863|9401202309';
 /** @type {string} */
 const CACHE_PREFIX = 'RankMan++-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
